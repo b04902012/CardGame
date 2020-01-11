e2024ba4a82108ef90dccd42947ccf27f399a062
albert0liu0 <b04902012@ntu.edu.tw>
2020-01-11 13:16:02 +0800

    use custom image

--- template/game/scripts/htm5game.matchgame.js
+++ template/game/scripts/htm5game.matchgame.js
@@ -2,18 +2,30 @@
  * Created by mengdi on 2015/10/29.
  */
 
+function randomPermutation(array){
+    permutated_array = []
+    while(array.length > 0){
+        idx = Math.floor(Math.random()*array.length)
+        permutated_array.push(array[idx])
+        array.splice(idx,1)
+    }
+    return permutated_array
+}
+
 var neusoft={};
 neusoft.matchingGame={};
-neusoft.matchingGame.cardWidth=80;neusoft.matchingGame.cardHeight=120;
-neusoft.matchingGame.deck=
-[
-    "cardAK","cardAK",
-    "cardAQ","cardAQ",
-    "cardAJ","cardAJ",
-    "cardBK","cardBK",
-    "cardBQ","cardBQ",
-    "cardBJ","cardBJ"
-]
+neusoft.matchingGame.cardWidth=250;neusoft.matchingGame.cardHeight=250;
+var score=0;
+var cards=[];
+for(var i=0;i<52;i++){
+    cards.push(("0"+i.toString()).slice(-2));
+}
+neusoft.matchingGame.deck=[]
+for(var i=0;i<15;i++){
+    neusoft.matchingGame.deck.push('picture'+cards[i]);
+    neusoft.matchingGame.deck.push('text'+cards[i]);
+}
+neusoft.matchingGame.deck=randomPermutation(neusoft.matchingGame.deck)
 function shuffle(){
     return Math.random()>0.5 ? -1 : 1
 }
@@ -34,10 +46,11 @@ function checkPattern(cards){
     var pattern1 = $(cards[0]).data("pattern");
     var pattern2 = $(cards[1]).data("pattern");
     $(cards).removeClass("card-flipped");
-    if(pattern1==pattern2){
+    if(pattern1.slice(-2)==pattern2.slice(-2)){
         $(cards).addClass("card-removed")
         .bind("webkitTransitionEnd",function(){
             $(this).remove();
+            score++;
         });
     }
 }
